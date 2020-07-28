dfeec2944c8459b0e44f7e340f72169c9916e1bc
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2020-07-28 14:32:00 +0300

    fix

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
+app.use("favicon.ico", express.static(__dirname + "/build/favicon.ico"));
 
 app.use(express.static(path.resolve(__dirname, "build")));
 
